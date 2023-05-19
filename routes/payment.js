e50f3549a67792670b89b8345326f300307d24b2
RUSHi-VAiRALE <rushivairale932@gmail.com>
2023-05-20 00:04:40 +0530

    added payment on singleImg

--- routes/payment.js
+++ routes/payment.js
@@ -6,7 +6,7 @@ const router = require("express").Router();
 router.post("/orders",(req,res)=>{
 let instance = new Razorpay({ key_id: process.env.RAZ_ID, key_secret: process.env.RAZ_SECRET })
 const amt = req.body.amount;
-const amt1 = amt.split(" ")[1];
+const amt1 = amt.split(" ")[1] * req.body.quantity;
 console.log(amt1)
 let options = {
     amount: Number(amt1 * 100),  // amount in the smallest currency unit
